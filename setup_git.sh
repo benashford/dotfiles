303df064b769112a521f96ca3f2f6a9099a0a815
Ben Ashford <ben.ashford@agentsmutual.co.uk>
2014-05-06 09:58:38 +0100

    Setup default push

--- setup_git.sh
+++ setup_git.sh
@@ -3,3 +3,5 @@
 git config --global alias.co "checkout"
 git config --global alias.st "status"
 git config --global alias.hist "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short"
+
+git config --global push.default simple
