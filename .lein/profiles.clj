9cb1a6441629bddfb86168ba5b49b63da2fa247d
Ben Ashford <ben.ashford@agentsmutual.co.uk>
2014-09-19 10:59:28 +0100

    Humane test output

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
-                  [lein-cloverage "1.0.2"]]}}
+                  [lein-cloverage "1.0.2"]]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
