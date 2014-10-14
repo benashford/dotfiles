78f689c84ab57238ac1f82886f5017a417d72057
Ben Ashford <ben.ashford@agentsmutual.co.uk>
2014-10-14 09:54:13 +0100

    Add refactor-nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
-                  [lein-cloverage "1.0.2"]]
+                  [lein-cloverage "1.0.2"]
+                  [refactor-nrepl "0.1.0"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
