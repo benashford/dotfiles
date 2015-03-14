738fb4130f14f91b1db33971bea7aa9651b17495
Ben Ashford <ben@bcash.org>
2015-03-14 15:00:53 +0000

    Removing unused stuff

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-cloverage "1.0.2"]
-                  [refactor-nrepl "0.1.0"]]
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
