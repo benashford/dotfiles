c64276b0361a0da80da2551f8cd6eac702891720
Ben Ashford <benashford@users.noreply.github.com>
2015-04-04 18:56:52 +0100

    Add lein-ancient

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-ancient "0.6.5"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
