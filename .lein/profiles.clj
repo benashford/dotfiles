18b558e68b9d851a1dda0d7a56d40797d7e84ce9
Ben Ashford <benashford@users.noreply.github.com>
2015-04-15 20:14:44 +0100

    Lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.6.5"]]
+                  [lein-ancient "0.6.5"]
+                  [lein-pprint "1.1.1"]
+                  [refactor-nrepl "1.0.1"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
