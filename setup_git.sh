d7b0afe9cb5c72c50c8c0ab9e19e54f2c24ecf83
Ben Ashford <benashford@users.noreply.github.com>
2015-05-02 21:25:42 +0100

    Forgotten things

--- setup_git.sh
+++ setup_git.sh
@@ -4,4 +4,7 @@ git config --global alias.co "checkout"
 git config --global alias.st "status"
 git config --global alias.hist "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short"
 
+git config --global user.name "Ben Ashford"
+git config --global user.email "benashford@users.noreply.github.com"
+
 git config --global push.default simple
