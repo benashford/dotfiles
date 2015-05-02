21e6b8cc464b67cd1a388af6cba4fcd0b944f58e
Ben Ashford <benashford@users.noreply.github.com>
2015-05-02 17:28:01 +0100

    Changes to minimal profile to allow project specific additions

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+mkdir .profiles
+
 cp dotfiles/.bash_profile_minimal .bash_profile
-cp dotfiles/.bashrc_minimal .bash_profile
+cp dotfiles/.profile_minimal .profile
+cp dotfiles/.bashrc_minimal .bashrc
 cp dotfiles/.tmux.conf .tmux.conf
 
 dotfiles/setup_git.sh
