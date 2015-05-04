04bd36d23a19bbd82cd24081a57180f91d380ab8
Ben Ashford <benashford@users.noreply.github.com>
2015-05-04 12:43:53 +0100

    Fix miscellaneous Leiningen conflicts

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,8 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.6.5"]
-                  [lein-pprint "1.1.1"]
-                  [refactor-nrepl "1.0.1"]]
-        :dependencies [[pjstadig/humane-test-output "0.6.0"]]
+                  [lein-ancient "0.6.7"]
+                  [lein-pprint "1.1.2"]
+                  [refactor-nrepl "1.0.5" :exclusions [org.clojure/clojure]]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                       [pjstadig/humane-test-output "0.7.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
