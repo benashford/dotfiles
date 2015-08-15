ff694e7767ba2c54d07295d3c2456386e4c7e19d
Ben Ashford <benashford@users.noreply.github.com>
2015-08-15 21:55:40 +0100

    Don't know how refactor-nrepl stops a very small number of protocols from working, but it does, so it's gone.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [lein-ancient "0.6.7"]
-                  [lein-pprint "1.1.2"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
+                  [lein-pprint "1.1.2"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                        [pjstadig/humane-test-output "0.7.0"]]
         :injections [(require 'pjstadig.humane-test-output)
