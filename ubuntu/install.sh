a5e56e1b66eae0c4ed44866e09ada8f79269ddf3
Ben Ashford <benashford@users.noreply.github.com>
2015-12-22 18:50:34 +0000

    Keymappings for the meta-key

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -5,6 +5,7 @@ mkdir .profiles
 cp dotfiles/ubuntu/.bash_profile .bash_profile
 cp dotfiles/ubuntu/.profile .profile
 cp dotfiles/ubuntu/.bashrc .bashrc
+cp dotfiles/ubuntu/.xmodmap .xmodmap
 cp dotfiles/ubutnu/.xprofile .xprofile
 cp dotfiles/.tmux.conf .tmux.conf
 
