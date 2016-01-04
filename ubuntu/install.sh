12a4a1a98fe5470b4dfb4ff1a8d95c67398748e4
Ben Ashford <benashford@users.noreply.github.com>
2016-01-04 19:58:03 +0000

    Typo

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -6,7 +6,7 @@ cp dotfiles/ubuntu/.bash_profile .bash_profile
 cp dotfiles/ubuntu/.profile .profile
 cp dotfiles/ubuntu/.bashrc .bashrc
 cp dotfiles/ubuntu/.xmodmap .xmodmap
-cp dotfiles/ubutnu/.xprofile .xprofile
+cp dotfiles/ubuntu/.xprofile .xprofile
 cp dotfiles/.tmux.conf .tmux.conf
 
 dotfiles/setup_git.sh
