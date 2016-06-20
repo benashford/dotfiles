afd36bc11f6e42eb7eb6f56bd21bfe8b8597d0c5
Ben Ashford <benashford@users.noreply.github.com>
2016-06-20 11:08:45 +0100

    Upgrade lein-ancient

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
-                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure]]
+                  [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
                   [lein-pprint "1.1.2"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [pjstadig/humane-test-output "0.7.0"]
