a4eba18eea5f0a367369974e1fdef4b3ddd31a02
Ben Ashford <benashford@users.noreply.github.com>
2016-07-30 19:37:51 +0100

    Use pre-installed tmux

--- ubuntu/install_tmux.sh
+++ ubuntu/install_tmux.sh
@@ -4,7 +4,7 @@ set -e
 
 # Pre-requisited
 
-apt-get -y install libevent-dev libncurses5-dev
+apt-get -y install tmux libevent-dev libncurses5-dev
 
 # tmux-mem-cpu-load
 
@@ -16,16 +16,3 @@ make
 make install
 popd
 rm -rf /usr/src/tmux-mem-cpu-load
-
-# Tmux itself
-
-mkdir -p /usr/src/tmux
-pushd /usr/src/tmux
-curl -L https://github.com/tmux/tmux/releases/download/2.1/tmux-2.1.tar.gz > tmux-2.1.tar.gz
-tar zxvf tmux-2.1.tar.gz
-pushd tmux-2.1
-./configure && make
-make install
-popd
-popd
-rm -rf /usr/src/tmux
